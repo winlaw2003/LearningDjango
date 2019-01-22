73729f59b80b401532f8879485d929068ced5eb1
jay law <jl@workprocess.co.uk>
2019-01-22 16:58:11 +0000

    Template example

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
 
 # Create your views here.
