65516974351da97f767873326d01767bf6020ec0
jay law <jl@workprocess.co.uk>
2019-01-22 09:58:13 +0000

    Hello Django

--- BasicProject/BasicProject/settings.py
+++ BasicProject/BasicProject/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Eurpoe/London'
 
 USE_I18N = True
 
